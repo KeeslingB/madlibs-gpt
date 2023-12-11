d6813496fe69f9b0680cd236fc2e7a6294aefaf4
bpavlis <benpavlis@gmail.com>
2023-12-11 12:40:13 -0600

    more styling changes/options to private page

--- client/pages/PrivatePage.jsx
+++ client/pages/PrivatePage.jsx
@@ -72,7 +72,20 @@ export default function PrivatePage(props) {
               </Card.Text>
             </Card.Body>
           </Card>
-          <Card style={{ width: '23rem', height: '600px', color: 'white', margin: '10px', background: 'purple' }}>
+        </div>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div id="card-container" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
+          <Card style={{ width: '100%', height: '250px', color: 'white', margin: '10px', background: 'purple' }}>
+            <Card.Body>
+              <Card.Title>Story Title</Card.Title>
+              <Card.Text>
+                Today, my fabulous camp group went to a (an) $$$$$$$ amusement park. It was a fun park with lots of cool $$$$$$$ and enjoyable play structures. When we got there, my kind counselor shouted loudly, "Everybody off the $$$$$$$." We all pushed out in a terrible hurry. My counselor handed out yellow tickets, and we scurried in. I was so excited! I couldn't figure out what exciting thing to do first. I saw a scary roller coaster I really liked so, I $$$$$$$ ran over to get in the long line that had about $$$$$$$ people in it. When I finally got on the roller coaster I was $$$$$$$. In fact I was so nervous my two knees were knocking together. This was the $$$$$$$ ride I had ever been on! In about two minutes I heard the crank and grinding of the gears. Thats when the ride began! When I got to the bottom, I was a little $$$$$$$ but I was proud of myself. The rest of the day went $$$$$$$. It was a(n) $$$$$$$ day at the fun park.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '100%', height: '250px', color: 'white', margin: '10px', background: 'purple' }}>
             <Card.Body>
               <Card.Title>Story Title</Card.Title>
               <Card.Text>
@@ -81,7 +94,25 @@ export default function PrivatePage(props) {
               </Card.Text>
             </Card.Body>
           </Card>
-          <Card style={{ width: '23rem', height: '600px', color: 'white', margin: '10px', background: 'purple' }}>
+          <Card style={{ width: '100%', height: '250px', color: 'white', margin: '10px', background: 'purple' }}>
+            <Card.Body>
+              <Card.Title>Story Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores delectus enim facilis rem atque qui architecto magnam in, vitae voluptatum placeat, debitis possimus soluta dolor officiis repudiandae velit nesciunt. Praesentium!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '100%', height: '250px', color: 'white', margin: '10px', background: 'purple' }}>
+            <Card.Body>
+              <Card.Title>Story Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '100%', height: '250px', color: 'white', margin: '10px', background: 'purple' }}>
             <Card.Body>
               <Card.Title>Story Title</Card.Title>
               <Card.Text>
