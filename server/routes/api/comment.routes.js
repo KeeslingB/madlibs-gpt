b1c276288a4cbe301ab87d374a5ba73bb246f2de
molly <molly.salzberger@gmail.com>
2023-12-12 12:27:08 -0600

    comment routes written. time to test

--- server/routes/api/comment.routes.js
+++ server/routes/api/comment.routes.js
@@ -7,3 +7,11 @@ const { getAllComments, getCommentsByStory, getSingleComment, writeComment, edit
 
 // path definitions
 router.route("/").get(getAllComments);
+router.route("/:id").get(getSingleComment);
+router.route("/:storyId/:id").get(getCommentsByStory);
+// full path SHOULD end up being api/comment/storyID
+router.route("/:storyId/").post(writeComment);
+router.route("/:id").put(editComment);
+router.route("/:id").delete(deleteComment);
+
+module.exports = router;
