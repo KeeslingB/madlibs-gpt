1c3c16571e758798fb172372f98d7ed0bcaf3b14
molly <molly.salzberger@gmail.com>
2023-12-12 11:09:57 -0600

    remembering try/catch

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -4,30 +4,54 @@ const ObjectId = require('mongoose').Types;
 
 // finding comments
 async function getAllComments (req, res) {
+    try {
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 async function getCommentsByStory (req, res) {
+    try {
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 async function getSingleComment (req, res) {
+    try {
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 // make a comment
 async function writeComment (req, res) {
+    try {
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 // update comment
 async function editComment (req, res) {
+    try {
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
 };
 
 // delete a comment
 async function deleteComment (req, res) {
+    try {
 
+    } catch (err) {
+        res.status(500).json(err);
+    }
 }
 
 module.exports = { getAllComments, getCommentsByStory, getSingleComment, writeComment, editComment, deleteComment }
