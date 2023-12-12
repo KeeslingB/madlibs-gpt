4ff1bed13cff0b87845f14e183907e1035168750
molly <molly.salzberger@gmail.com>
2023-12-12 10:32:58 -0600

    moved files, changed import statements

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useAppCtx } from "../utils/AppProvider"
-import "../public/assests/Header.css"
+import "../Header.css"
 
 export default function Header() {
   const { user } = useAppCtx()
