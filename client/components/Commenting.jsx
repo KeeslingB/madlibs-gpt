e9f6771e53eb170ec0cde9b43a237e78cdb5b08a
molly <molly.salzberger@gmail.com>
2023-12-13 10:28:21 -0600

    more comment form skeleton

--- client/components/Commenting.jsx
+++ client/components/Commenting.jsx
@@ -8,7 +8,18 @@ export default function Commenting(){
     const [commentData, setCommentData] = useState({ story: "", commenterName: "", commentText: "", });
 
     function handleInputChange(e) {
-        setCommentData({...commentData, })
+        setCommentData({...commentData, [e.target.name]: e.target.value })
+    };
+
+    async function sendForm(){
+
+
+        try {
+            console.log("sending comment...")
+
+        } catch (err) {
+            console.log(err.message)
+        }
     }
 
     return
