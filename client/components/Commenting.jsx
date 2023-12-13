f3283ae3fb22214537a755cdcc183c1a06f27892
molly <molly.salzberger@gmail.com>
2023-12-13 10:25:14 -0600

    saving progress just in case (computer iffy today)

--- client/components/Commenting.jsx
+++ client/components/Commenting.jsx
@@ -5,7 +5,11 @@ import { useEffect, useState } from "react"
 
 export default function Commenting(){
 
-    const [commentData, setCommentData] = useState({})
+    const [commentData, setCommentData] = useState({ story: "", commenterName: "", commentText: "", });
+
+    function handleInputChange(e) {
+        setCommentData({...commentData, })
+    }
 
     return
 
